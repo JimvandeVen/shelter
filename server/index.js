000875e240071b096bdaa9f166a6c3dfca7cf442
root <root@DESKTOP-V5IGRM1.localdomain>
2018-03-13 00:49:13 +0100

    added the upload(form) page. Made it so the user can add animals to the database and have a new page for the added animals

--- server/index.js
+++ server/index.js
@@ -1,22 +1,28 @@
 'use strict'
 
 var express = require('express')
+var bodyParser = require('body-parser')
 var db = require('../db')
 var helpers = require('./helpers')
 
 module.exports = express()
     .set('view engine', 'ejs')
     .set('views', 'view')
+    .use(bodyParser.urlencoded({
+        extended: true
+    }))
+    .use(bodyParser.json())
     .use(express.static('static'))
     .use('/image', express.static('./db/image'))
     // TODO: Serve the images in `db/image` on `/image`.
     .get('/', all)
+    .get('/upload', upload)
     /* TODO: Other HTTP methods. */
-    // .post('/', add)
+    .post('/', add)
     .get('/:id', get)
     // .put('/:id', set)
     // .patch('/:id', change)
-     .delete('/:id', remove)
+    .delete('/:id', remove)
     .listen(1902)
 
 function all(req, res) {
@@ -35,7 +41,6 @@ function all(req, res) {
 
 function get(req, res) {
     var id = req.params.id
-    var removed = db.removed(id)
     var has
     var result = {
         errors: [],
@@ -70,7 +75,7 @@ function get(req, res) {
             html: () => res.render('detail.ejs', Object.assign({}, result, helpers))
         })
 
-    } else if (removed) {
+    } else if (db.removed(id)) {
         result = {
             errors: [
                 {
@@ -86,7 +91,7 @@ function get(req, res) {
             html: () => res.render('error.ejs', Object.assign({}, result, helpers))
         })
 
-    }else {
+    } else {
         // 404 not found
         result = {
             errors: [
@@ -105,6 +110,11 @@ function get(req, res) {
 
     }
 }
+
+function upload(req, res) {
+    res.render('upload.html')
+}
+
 
 function remove(req, res) {
     var id = req.params.id
@@ -172,10 +182,66 @@ function remove(req, res) {
     }
 }
 
+function add(req, res) {
+    var result = {
+        errors: [],
+        data: undefined
+    }
+    var addAnimal = {
+        name: req.body.name,
+        type: req.body.type,
+        place: req.body.place,
+        description: req.body.description,
+        sex: req.body.sex,
+        age: parseInt(req.body.age, 10),
+        size: req.body.size,
+        length: req.body.length,
+        vaccinated: req.body.vaccinated == 1,
+        declawed: req.body.declawed,
+        coat: req.body.coat,
+        primaryColor: req.body.primaryColor,
+        secondaryColor: req.body.secondaryColor,
+        weight: parseInt(req.body.weight, 10),
+        intake: req.body.intake
+    }
+    if (addAnimal.type === 'dog' || addAnimal.type === 'rabbit') {
+        addAnimal.declawed = undefined
+    } else if (addAnimal.type === 'cat' || addAnimal.type != undefined) {
+        addAnimal.declawed = 'true'
+    } else {
+        addAnimal.declawed = undefined
+    }
+
+    if (addAnimal.secondaryColor === '' || addAnimal.secondaryColor === undefined) {
+        addAnimal.secondaryColor = undefined
+    }
 
 
-/* Support both a request for JSON and a request for HTML  */
-// res.format({
-//   json: () => res.json(result),
-//   html: () => res.render('list.ejs', Object.assign({}, result, helpers))
-// })
+    try {
+        var newAnimal = db.add(addAnimal)
+        res.format({
+            json: () => res.json(result),
+            html: () => res.redirect('/' + newAnimal.id)
+        })
+        console.log('added')
+    } catch (err) {
+        console.log(addAnimal)
+        result = {
+            errors: [
+                {
+                    id: 422,
+                    title: 'Uprocessable Entity',
+                    description: 'Uprocessable Entity',
+                    detail: 'detail'
+                }
+        ]
+        }
+        res.format({
+            json: () => res.json(result),
+            html: () => res.render('error.ejs', Object.assign({}, result, helpers))
+        })
+
+
+
+    }
+}
