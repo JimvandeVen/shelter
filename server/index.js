fdbaad82dabbff361f3163424b1b379ba46d32fe
root <root@DESKTOP-V5IGRM1.localdomain>
2018-03-21 14:00:24 +0100

    working on the errors

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ var connection = mysql.createConnection({
 })
 
 connection.connect()
-console.log(connection)
+console.log('Server is Listening')
 
 module.exports = express()
     .set('view engine', 'ejs')
@@ -41,88 +41,171 @@ module.exports = express()
     .listen(1902)
 
 function all(req, res, next) {
-  connection.query('SELECT * FROM movies', done)
+    connection.query('SELECT * FROM animals', done)
 
-  function done(err, data) {
-    if (err) {
-      next(err)
-    } else {
-      res.render('list.ejs', {data: data})
+    function done(err, data) {
+        if (err) {
+            next(err)
+        } else {
+            res.render('list.ejs', {
+                data: data
+            })
+        }
     }
-  }
 }
-function get(req, res) {
+
+//function get(req, res, next) {
+//  var slug = req.params.slug
+//  connection.query(`SELECT * FROM animals ${joins('animals')} WHERE animals.slug = '${slug}'`, done)
+//
+//  function done(err, data) {
+//    if (err) {
+//      console.error(err)
+//      if (err.code == 'ER_PARSE_ERROR') {
+//        onerror([400], res)
+//      }
+//    } else if (!data.length) {
+//      onerror([404], res)
+//    } else {
+//      var result = {
+//        data: data[0]
+//      }
+//      res.format({
+//        json: () => res.json(result),
+//        html: () => res.render('detail.ejs', Object.assign({}, result, helpers))
+//      })
+//    }
+//  }
+//}
+
+function get(req, res, next) {
     var id = req.params.id
-    var has
-    var result = {
-        errors: [],
-        data: undefined
-    }
-    try {
-        has = db.has(id)
-    } catch (err) {
-        // 400 invalid id
-        result = {
-            errors: [
-                {
-                    id: 400,
-                    title: 'Bad Request',
-                    description: 'Bad Request',
-                    detail: 'detail'
+    var badRequest = isNaN(id)
+    console.log(badRequest)
+    connection.query('SELECT * FROM animals WHERE id = ?', id, done)
+
+    function done(err, data) {
+        if (err) {
+            console.error(err)
+            if (!badRequest) {
+                result = {
+                    errors: [
+                        {
+                            id: 400,
+                            title: 'Bad Request',
+                            description: 'Bad Request',
+                            detail: 'detail'
                 }
         ]
-        }
-        res.format({
-            json: () => res.json(result),
-            html: () => res.render('error.ejs', Object.assign({}, result, helpers))
-        })
-        return
-
-    }
-
-    if (has) {
-        result.data = db.get(id)
-        // 200 ok
-        res.format({
-            json: () => res.json(result),
-            html: () => res.render('detail.ejs', Object.assign({}, result, helpers))
-        })
-
-    } else if (db.removed(id)) {
-        result = {
-            errors: [
-                {
-                    id: 410,
-                    title: 'Gone',
-                    description: 'This animal was removed',
-                    detail: 'detail'
+                }
+                res.format({
+                    json: () => res.json(result),
+                    html: () => res.render('error.ejs', Object.assign({}, result, helpers))
+                })
+            }
+            return
+        } else if (id.length != 5 || data.length === 0) {
+            // 404 not found
+            result = {
+                errors: [
+                    {
+                        id: 404,
+                        title: 'Page Not Found',
+                        description: 'This animal does not exist',
+                        detail: 'detail'
                 }
         ]
+            }
+            res.format({
+                json: () => res.json(result),
+                html: () => res.render('error.ejs', Object.assign({}, result, helpers))
+            })
+            return
+        } else {
+            var result = {
+                data: data[0]
+            }
+            res.format({
+                json: () => res.json(result),
+                html: () => res.render('detail.ejs', Object.assign({}, result, helpers))
+            })
         }
-        res.format({
-            json: () => res.json(result),
-            html: () => res.render('error.ejs', Object.assign({}, result, helpers))
-        })
-
-    } else {
-        // 404 not found
-        result = {
-            errors: [
-                {
-                    id: 404,
-                    title: 'Page Not Found',
-                    description: 'This animal does not exist',
-                    detail: 'detail'
-                }
-        ]
-        }
-        res.format({
-            json: () => res.json(result),
-            html: () => res.render('error.ejs', Object.assign({}, result, helpers))
-        })
-
     }
 }
+
+
+//function get(req, res) {
+//    var id = req.params.id
+//    var has
+//    var result = {
+//        errors: [],
+//        data: undefined
+//    }
+//    try {
+//        has = db.has(id)
+//    } catch (err) {
+//        // 400 invalid id
+//        result = {
+//            errors: [
+//                {
+//                    id: 400,
+//                    title: 'Bad Request',
+//                    description: 'Bad Request',
+//                    detail: 'detail'
+//                }
+//        ]
+//        }
+//        res.format({
+//            json: () => res.json(result),
+//            html: () => res.render('error.ejs', Object.assign({}, result, helpers))
+//        })
+//        return
+//
+//    }
+//
+//    if (has) {
+//        result.data = db.get(id)
+//        // 200 ok
+//        res.format({
+//            json: () => res.json(result),
+//            html: () => res.render('detail.ejs', Object.assign({}, result, helpers))
+//        })
+//
+//    } else if (db.removed(id)) {
+//        result = {
+//            errors: [
+//                {
+//                    id: 410,
+//                    title: 'Gone',
+//                    description: 'This animal was removed',
+//                    detail: 'detail'
+//                }
+//        ]
+//        }
+//        res.format({
+//            json: () => res.json(result),
+//            html: () => res.render('error.ejs', Object.assign({}, result, helpers))
+//        })
+//
+//    } else {
+//        // 404 not found
+//        result = {
+//            errors: [
+//                {
+//                    id: 404,
+//                    title: 'Page Not Found',
+//                    description: 'This animal does not exist',
+//                    detail: 'detail'
+//                }
+//        ]
+//        }
+//        res.format({
+//            json: () => res.json(result),
+//            html: () => res.render('error.ejs', Object.assign({}, result, helpers))
+//        })
+//
+//    }
+//}
 
 function upload(req, res) {
     res.render('upload.html')
@@ -196,11 +279,7 @@ function remove(req, res) {
 }
 
 function add(req, res) {
-    var result = {
-        errors: [],
-        data: undefined
-    }
-    var addAnimal = {
+    connection.query('INSERT INTO animals SET ?', {
         name: req.body.name,
         type: req.body.type,
         place: req.body.place,
@@ -215,8 +294,9 @@ function add(req, res) {
         primaryColor: req.body.primaryColor,
         secondaryColor: req.body.secondaryColor,
         weight: parseInt(req.body.weight, 10),
-        intake: req.body.intake
-    }
+        date: req.body.intake
+    }, done)
+
     if (addAnimal.type === 'dog' || addAnimal.type === 'rabbit') {
         addAnimal.declawed = undefined
     } else if (addAnimal.type === 'cat' || addAnimal.type != undefined) {
@@ -229,33 +309,89 @@ function add(req, res) {
         addAnimal.secondaryColor = undefined
     }
 
-
-    try {
-        var newAnimal = db.add(addAnimal)
-        res.format({
-            json: () => res.json(result),
-            html: () => res.redirect('/' + newAnimal.id)
-        })
-        console.log('added')
-    } catch (err) {
-        console.log(addAnimal)
-        result = {
-            errors: [
-                {
-                    id: 422,
-                    title: 'Uprocessable Entity',
-                    description: 'Uprocessable Entity',
-                    detail: 'detail'
-                }
-        ]
-        }
-        res.format({
-            json: () => res.json(result),
-            html: () => res.render('error.ejs', Object.assign({}, result, helpers))
-        })
-
-
-
+    function done(err, data) {
+        console.log(err)
+        res.redirect('/' + data.insertId)
+        //        try {
+        //            var newAnimal = db.add(addAnimal)
+        //            res.redirect('/' + data.insertId)
+        //            console.log('added')
+        //        } catch (err) {
+        //            result = {
+        //                errors: [
+        //                    {
+        //                        id: 422,
+        //                        title: 'Uprocessable Entity',
+        //                        description: 'Uprocessable Entity',
+        //                        detail: 'detail'
+        //                }
+        //        ]
+        //            }
+        //            res.format({
+        //                json: () => res.json(result),
+        //                html: () => res.render('error.ejs', Object.assign({}, result, helpers))
+        //            })
+        //
+        //
+        //
+        //        }
     }
 }
 
+//try {
+//    var newAnimal = db.add(addAnimal)
+//    res.redirect('/' + data.insertId)
+//    console.log('added')
+//} catch (err) {
+//    console.log(addAnimal)
+//    result = {
+//        errors: [
+//            {
+//                id: 422,
+//                title: 'Uprocessable Entity',
+//                description: 'Uprocessable Entity',
+//                detail: 'detail'
+//                }
+//        ]
+//    }
+//    res.format({
+//        json: () => res.json(result),
+//        html: () => res.render('error.ejs', Object.assign({}, result, helpers))
+//    })
+//
+//
+//
+//}
+
+
+//        function done(err, data) {
+//            if (addAnimal.type === 'dog' || addAnimal.type === 'rabbit') {
+//            addAnimal.declawed = undefined
+//        } else if (addAnimal.type === 'cat' || addAnimal.type != undefined) {
+//            addAnimal.declawed = 'true'
+//        } else {
+//            addAnimal.declawed = undefined
+//        }
+//
+//    if (addAnimal.secondaryColor === '' || addAnimal.secondaryColor === undefined) {
+//        addAnimal.secondaryColor = undefined
+//    }
+//    if (err) {
+//        next(err)
+//    } else {
+//        res.redirect('/' + data.insertId)
+//    }
+//}
+//
+//if (addAnimal.type === 'dog' || addAnimal.type === 'rabbit') {
+//    addAnimal.declawed = undefined
+//} else if (addAnimal.type === 'cat' || addAnimal.type != undefined) {
+//    addAnimal.declawed = 'true'
+//} else {
+//    addAnimal.declawed = undefined
+//}
+//
+//if (addAnimal.secondaryColor === '' || addAnimal.secondaryColor === undefined) {
+//    addAnimal.secondaryColor = undefined
+//}
+//
